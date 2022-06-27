a7b4cecab9f07e2a6c44f722287f263f9179db1d
SlavaSadykov <108270836+SlavaSadykov@users.noreply.github.com>
2022-06-27 15:32:19 +0500

    Update 04-4.py

--- 04-4.py
+++ 04-4.py
@@ -1,5 +1,7 @@
 class Date:
-  def __init__(self, m, day):
+  def __init__(self, month, day):
     self.month = month
     self.day = day
-
+  def __sub__(self, other):
+     dt = d.date(2022, self.month, self.day)
+    
