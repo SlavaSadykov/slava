3c7b9eb777bc6ffb896ea0e07ac8a56dd391b38a
SlavaSadykov <108270836+SlavaSadykov@users.noreply.github.com>
2022-06-27 15:28:31 +0500

    Update 04-6.py

--- 04-6.py
+++ 04-6.py
@@ -3,4 +3,14 @@ class SparseArray:
     self.massiv = {}
 
   def __setitem__(self, key, value):
+     self.massiv[key] = value
 
+  def __getitem__(self, key):
+     return self.massiv.get(key, 0)
+
+    
+arr = SparseArray()
+arr[1] = 10
+arr[8] = 20
+for i in range(10):
+    print('arr[{}] = {}'.format(i, arr[i]))
