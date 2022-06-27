ce67d0cfd51435c6ccdf3aa744082cf83851d915
SlavaSadykov <108270836+SlavaSadykov@users.noreply.github.com>
2022-06-27 15:36:40 +0500

    Update 04-4.py

--- 04-4.py
+++ 04-4.py
@@ -4,4 +4,16 @@ class Date:
     self.day = day
   def __sub__(self, other):
      dt = d.date(2022, self.month, self.day)
-    
+     dt_new = d.date(2022, other.month, other.day)
+       if dt == dt_new:
+          return 0
+       return  ((str(dt - dt_new)).split())[0]
+jan5 = Date(1, 5)
+jan1 = Date(1, 1)
+
+print(jan5 - jan1)
+print(jan1 - jan5)
+print(jan1 - jan1)
+print(jan5 - jan5)
+print()
+
